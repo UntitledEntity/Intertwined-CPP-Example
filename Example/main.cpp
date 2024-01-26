877eaf24eaa09743f9f8153aab04f06a61c2a298
Jacob Gluska <3e18amn20@protonmail.com>
2024-01-26 16:07:10 -0500

    Add register option to main

--- Example/main.cpp
+++ Example/main.cpp
@@ -24,17 +24,41 @@ int main( )
         return 0;
     }
 
-    int loginchoice = 0;
+    int choice = 0;
 
-    std::cout << "Choose login type, License (0), User & Pass (1): ";
-    std::cin >> loginchoice;
+    std::cout << "Choose action: License (0), User & Pass (1), Register (2): ";
+    std::cin >> choice;
 
-    if ( loginchoice != 0 && loginchoice != 1 ) {
+    if ( choice != 0 && choice != 1 && choice != 2) {
         std::cout << "Please choose 0 or 1";
         return 0;
     }
 
-    if ( loginchoice == 1 ) {
+    if ( choice == 2 ) {
+        std::string License, User, Pass;
+
+        std::cout << "License: ";
+        std::cin >> License;
+
+        std::cout << "Username: ";
+        std::cin >> User;
+
+        std::cout << "Pass: ";
+        std::cin >> Pass;
+
+        std::optional<IntertwinedAuth::UserData> RegisterResp = Auth.Register( User, Pass, License );
+
+        if ( !RegisterResp ) {
+            std::cout << "An unexepected error has occured: " << Auth.GetLastError( );
+            return 0;
+        }
+
+        IntertwinedAuth::UserData Userdata = RegisterResp.value( );
+
+        std::cout << "User " << Userdata.Username << " registered with license " << License << "\n";
+
+    }
+    else if ( choice == 1 ) {
 
         std::string User, Pass;
 
@@ -59,7 +83,7 @@ int main( )
 
         std::cout << "You are connecting from " << Userdata.IpAddress << " and you are level " << Userdata.Level << "\n";
     }
-    else if ( loginchoice == 0 ) {
+    else if ( choice == 0 ) {
         std::string License;
 
         std::cout << "License: ";
@@ -96,7 +120,7 @@ int main( )
     }
 
     std::cout << "Variable return: " + VarRet.value( ) + "\n";*/
-
+    
     // This closes out the session. The sessions which are more than 24 hours long will terminate automatically, but you should still terminate them after you're done.
     Auth.Close( );
 
